0d63dcadd533c0165fda10c445bafaefed4ce6f9
peterfam <peter.fam@advansys-esc.com>
2024-03-07 03:32:14 +0200

    - adding data layer - adding necessary files creating local database

---
+++ app/valifysdk/src/main/java/com/peterfam/valifaysdk/data/User.kt
@@ -0,0 +1,14 @@
+package com.peterfam.valifaysdk.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User(
+    val userName: String,
+    val password: String,
+    val mob: Int,
+    val email: String,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+)
---
+++ app/valifysdk/src/main/java/com/peterfam/valifaysdk/data/UsersDao.kt
@@ -0,0 +1,22 @@
+package com.peterfam.valifaysdk.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface UsersDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Query("SELECT * FROM user")
+    suspend fun getUsers(): LiveData<List<User>>
+
+}
