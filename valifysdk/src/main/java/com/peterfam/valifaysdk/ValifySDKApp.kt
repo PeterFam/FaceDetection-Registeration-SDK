d3981b9c19c1b1d01e030fb6208cd3f12ac65540
Peter Fam <peter_fam@hotmail.com>
2024-10-29 01:28:22 +0300

    - migrate dependence injection code from Hilt to Koin

--- valifysdk/src/main/java/com/peterfam/valifaysdk/ValifySDKApp.kt
+++ valifysdk/src/main/java/com/peterfam/valifaysdk/ValifySDKApp.kt
@@ -2,8 +2,10 @@ package com.peterfam.valifaysdk
 
 import android.app.Application
 import com.peterfam.valifaysdk.di.databaseModule
+import com.peterfam.valifaysdk.di.photoPicViewModelModule
+import com.peterfam.valifaysdk.di.registrationViewModelModule
 import com.peterfam.valifaysdk.di.repositoryModule
-import com.peterfam.valifaysdk.di.viewModelModule
+import com.peterfam.valifaysdk.di.userListViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -17,8 +19,9 @@ class ValifySDKApp : Application(){
             androidContext(this@ValifySDKApp)
             modules(databaseModule)
             modules(repositoryModule)
-            modules(viewModelModule)
-
+            modules(registrationViewModelModule)
+            modules(photoPicViewModelModule)
+            modules(userListViewModel)
         }
 
     }
--- valifysdk/src/main/java/com/peterfam/valifaysdk/di/AppModule.kt
+++ valifysdk/src/main/java/com/peterfam/valifaysdk/di/AppModule.kt
@@ -6,13 +6,22 @@ import com.peterfam.valifaysdk.data.UsersDao
 import com.peterfam.valifaysdk.data.UsersDatabase
 import com.peterfam.valifaysdk.domain.UserRepoImpl
 import com.peterfam.valifaysdk.domain.UsersRepo
-import com.peterfam.valifaysdk.presentation.viewmodel.RegistrationViewModel
+import com.peterfam.valifaysdk.presentation.screen.profile_pic_screen.viewmodel.PhotoPicViewModel
+import com.peterfam.valifaysdk.presentation.screen.registration_screen.viewmodel.RegistrationViewModel
+import com.peterfam.valifaysdk.presentation.screen.user_list.view_model.UserListViewModel
 import com.peterfam.valifaysdk.util.Constants
-import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModelModule = module {
-    viewModelOf(::RegistrationViewModel)
+val registrationViewModelModule = module {
+    viewModel {  RegistrationViewModel(get()) }
+}
+val photoPicViewModelModule = module {
+    viewModel { PhotoPicViewModel(get()) }
+}
+
+val userListViewModel = module {
+    viewModel { UserListViewModel(get()) }
 }
 
 fun provideRoomDatabase(application: Application) : UsersDatabase {
